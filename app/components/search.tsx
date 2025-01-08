bb53f42ef3fec855a7eb159dce17c5c252dfce5b
mandoo <mandoo@Jmandooui-MacBookAir.local>
2025-01-08 23:32:51 +0900

    modify search.tsx

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -7,7 +7,8 @@ import React, { useRef } from 'react';
 export default function Search() {
   const inputValue = useRef(null);
 
-  async function create(data) {
+  async function create() {
+    const data = inputValue.current.value;
     try {
       const response = await fetch("https://assignment-todolist-api.vercel.app/api/mandoo/items", {
         method: "POST",
@@ -25,18 +26,12 @@ export default function Search() {
       console.error("실패:", error);
     }
   }
-  
-  const clickHandle = event => {
-    event.preventDefault();
-    const todoStr = inputValue.current.value;
-    create(todoStr);
-   }
 
   return (
     <div className="justify-items-center py-4">
-      <Form action="/search" className="flex flex-row justify-between">
+      <Form action="/" className="flex flex-row justify-between">
         <input className={styles.search} ref={inputValue} name="memo" placeholder="할 일을 입력해주세요."/>
-        <button type="button" className="ml-2" onClick={clickHandle}>
+        <button type="button" className="ml-2" onClick={create}>
           <Image src="/btn/plusbtn.svg" alt="추가하기" width={150} height={56} /> 
         </button>
       </Form>
